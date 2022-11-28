42d582265af0ac186892de483e1e56cad1189283
Rishard C <rishard.c@hotmail.com>
2022-11-28 08:16:46 -0500

    remove curly braces from import statements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { React } from "react";
-import Square from "./components/Square";
-import Board from "./components/Board";
-import { Game } from "./components/Game";
+import Game from "./components/Game";
 
 const app = () => <Game/> 
 
